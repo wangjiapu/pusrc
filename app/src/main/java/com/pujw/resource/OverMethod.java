75a76916866bcaa3d4dd70e740dbd24a57068c72
pujiawang <pujiawang@sogou-inc.com>
2021-07-19 10:19:26 +0800

    代码查看

---
+++ app/src/main/java/com/pujw/resource/OverMethod.java
@@ -0,0 +1,13 @@
+package com.pujw.resource;
+
+public class OverMethod {
+
+    public int show(String s){
+        return 0;
+    }
+    //相同方法的判断没有返回值这条件
+//    public boolean show(String s){
+//        return false;
+//    }
+
+}
---
+++ app/src/main/java/com/pujw/resource/WeakCacheDemo.java
@@ -0,0 +1,11 @@
+package com.pujw.resource;
+
+import java.util.WeakHashMap;
+
+public class WeakCacheDemo {
+
+
+    public void display(){
+        //WeakChace<Object,Object> vdf=Wea
+    }
+}
